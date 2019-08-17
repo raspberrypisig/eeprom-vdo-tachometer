284983453a8065b4f4795aed609229fa24f61f9d
raspberrypisig <raspberrypisig@gmail.com>
2019-08-17 17:00:51 +1000

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,14 @@
+with open('arduino-eepromdump.txt') as f:
+  for line in f:
+    for i in line.split(' '):
+      if i == '':
+        continue
+      if int(i) == 0:
+        print('00', end='')
+      elif int(i) <= 9:
+        print('0', end='')
+        print(int(i), end='')
+      else:
+        print('{:x}'.format(int(i)), end='')
+      print(' ', end='')
+print()
