e36d3b1525fce5acb64891f2404d7e054abdfbbd
raspberrypisig <raspberrypisig@gmail.com>
2019-08-17 16:57:22 +1000

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+
+with open('read-data-short-convert.txt') as f:
+  for line in f:
+    if int(line) == 0:
+      print('00', end='')
+    elif int(line) <= 9 :
+      print('0', end='')
+      print(int(line), end='')
+      print(' ', end='')
+    else:
+      print(format(int(line), 'x'), end='')
+    print(' ', end='')
+print()
